cfc51a7133cdfe20091c91a970191f738b25fdc3
Ofori Michael <possigee86@gmail.com>
2025-08-12 21:49:22 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- next.config.ts
+++ next.config.ts
@@ -16,6 +16,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'images-prod.healthline.com',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
 };
